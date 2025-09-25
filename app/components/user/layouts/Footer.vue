06077f08afa2ea77f1b65de48207a1d1332a8868
charlyrm14 <charlyrm14@gmail.com>
2025-09-25 13:50:39 -0600

    feature(ui-classes): se agrega seccion de clases de alumno

--- app/components/user/layouts/Footer.vue
+++ app/components/user/layouts/Footer.vue
@@ -51,10 +51,36 @@
                         :class="route.path === '/my-progress' ? 'text-blue-500' : ''"> Progreso </span>
             </NuxtLink>
 
-            <a href="#" class="flex flex-col items-center justify-center text-sm space-y-1 hover:text-blue-500">
-                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar-days-icon lucide-calendar-days"><path d="M8 2v4"/><path d="M16 2v4"/><rect width="18" height="18" x="3" y="4" rx="2"/><path d="M3 10h18"/><path d="M8 14h.01"/><path d="M12 14h.01"/><path d="M16 14h.01"/><path d="M8 18h.01"/><path d="M12 18h.01"/><path d="M16 18h.01"/></svg>
-                <span class="font-light"> Clases </span>
-            </a>
+            <NuxtLink 
+                to="/my-classes"  
+                class="flex flex-col items-center justify-center text-sm space-y-1 hover:text-blue-500">
+                    <svg 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        width="28" 
+                        height="28" 
+                        viewBox="0 0 24 24" 
+                        fill="none" 
+                        stroke="currentColor" 
+                        :stroke-width="route.path === '/my-classes' ? 2 : 1" 
+                        stroke-linecap="round" 
+                        stroke-linejoin="round" 
+                        class="lucide lucide-calendar-days-icon lucide-calendar-days"
+                        :class="route.path === '/my-classes' ? 'text-blue-500' : ''">
+                            <path d="M8 2v4"/>
+                            <path d="M16 2v4"/>
+                            <rect width="18" height="18" x="3" y="4" rx="2"/>
+                            <path d="M3 10h18"/>
+                            <path d="M8 14h.01"/>
+                            <path d="M12 14h.01"/>
+                            <path d="M16 14h.01"/>
+                            <path d="M8 18h.01"/>
+                            <path d="M12 18h.01"/>
+                            <path d="M16 18h.01"/>
+                    </svg>
+                    <span 
+                        class="font-light"
+                        :class="route.path === '/my-classes' ? 'text-blue-500' : ''"> Clases </span>
+            </NuxtLink>
             
             <a href="#" class="flex flex-col items-center justify-center text-sm space-y-1 hover:text-blue-500">
                 <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-medal-icon lucide-medal"><path d="M7.21 15 2.66 7.14a2 2 0 0 1 .13-2.2L4.4 2.8A2 2 0 0 1 6 2h12a2 2 0 0 1 1.6.8l1.6 2.14a2 2 0 0 1 .14 2.2L16.79 15"/><path d="M11 12 5.12 2.2"/><path d="m13 12 5.88-9.8"/><path d="M8 7h8"/><circle cx="12" cy="17" r="5"/><path d="M12 18v-2h-.5"/></svg>
