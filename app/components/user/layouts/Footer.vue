67dc23ba2e34e1c2c40ace63ed33d91d2b557f70
charlyrm14 <charlyrm14@gmail.com>
2025-09-25 16:40:28 -0600

    feature(ui-user-profile): se agrega seccion perfil de usuario

--- app/components/user/layouts/Footer.vue
+++ app/components/user/layouts/Footer.vue
@@ -81,16 +81,29 @@
                         class="font-light"
                         :class="route.path === '/my-classes' ? 'text-blue-500' : ''"> Clases </span>
             </NuxtLink>
-            
-            <a href="#" class="flex flex-col items-center justify-center text-sm space-y-1 hover:text-blue-500">
-                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-medal-icon lucide-medal"><path d="M7.21 15 2.66 7.14a2 2 0 0 1 .13-2.2L4.4 2.8A2 2 0 0 1 6 2h12a2 2 0 0 1 1.6.8l1.6 2.14a2 2 0 0 1 .14 2.2L16.79 15"/><path d="M11 12 5.12 2.2"/><path d="m13 12 5.88-9.8"/><path d="M8 7h8"/><circle cx="12" cy="17" r="5"/><path d="M12 18v-2h-.5"/></svg>
-                <span class="font-light"> Logros </span>
-            </a>
-            
-            <a href="#" class="flex flex-col items-center justify-center text-sm space-y-1 hover:text-blue-500">
-                <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-user-icon lucide-user"><path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/><circle cx="12" cy="7" r="4"/></svg>
-                <span class="font-light"> Perfil </span>
-            </a>
+                        
+            <NuxtLink 
+                to="/profile" 
+                class="flex flex-col items-center justify-center text-sm space-y-1 hover:text-blue-500">
+                    <svg 
+                        xmlns="http://www.w3.org/2000/svg" 
+                        width="28" 
+                        height="28" 
+                        viewBox="0 0 24 24" 
+                        fill="none" 
+                        stroke="currentColor" 
+                        :stroke-width="route.path === '/profile' ? 2 : 1" 
+                        stroke-linecap="round" 
+                        stroke-linejoin="round" 
+                        class="lucide lucide-user-icon lucide-user"
+                        :class="route.path === '/profile' ? 'text-blue-500' : ''">
+                            <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2"/>
+                            <circle cx="12" cy="7" r="4"/>
+                    </svg>
+                    <span 
+                        class="font-light"
+                        :class="route.path === '/profile' ? 'text-blue-500' : ''"> Perfil </span>
+            </NuxtLink>
 
         </nav>
     </footer>
