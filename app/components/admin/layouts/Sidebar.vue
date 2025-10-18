d61841f8e666b777146e5108848c05b7fb283982
charlyrm14 <charlyrm14@gmail.com>
2025-10-17 23:01:25 -0600

    feature(ui-dashboard-contents): se agrega seccion lista de contenidos

--- app/components/admin/layouts/Sidebar.vue
+++ app/components/admin/layouts/Sidebar.vue
@@ -22,6 +22,15 @@
                     </li>
                     <li>
                         <NuxtLink 
+                            to="/kg-admin/contents" 
+                            class="flex items-center p-2 rounded-lg hover:text-blue-500 transition"
+                            :class="route.path === '/kg-admin/contents' ? 'bg-gray-100 dark:bg-dark-soft text-blue-500 font-semibold' : 'text-gray-500 dark:text-gray-400'">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-pin-icon lucide-pin mr-3"><path d="M12 17v5"/><path d="M9 10.76a2 2 0 0 1-1.11 1.79l-1.78.9A2 2 0 0 0 5 15.24V16a1 1 0 0 0 1 1h12a1 1 0 0 0 1-1v-.76a2 2 0 0 0-1.11-1.79l-1.78-.9A2 2 0 0 1 15 10.76V7a1 1 0 0 1 1-1 2 2 0 0 0 0-4H8a2 2 0 0 0 0 4 1 1 0 0 1 1 1z"/></svg>
+                                    Contenido
+                        </NuxtLink>
+                    </li>
+                    <li>
+                        <NuxtLink 
                             to="/kg-admin/users" 
                             class="flex items-center p-2 rounded-lg hover:text-blue-500 transition"
                             :class="route.path === '/kg-admin/users' ? 'bg-gray-100 dark:bg-dark-soft text-blue-500 font-semibold' : 'text-gray-500 dark:text-gray-400'">
