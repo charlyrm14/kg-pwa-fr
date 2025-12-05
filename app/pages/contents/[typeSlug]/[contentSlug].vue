1d948af4917341381c2e8d16406e5b52705788b2
charlyrm14 <charlyrm14@gmail.com>
2025-12-04 19:53:45 -0600

    feat(ui-animations): se agregan animaciones generales

--- app/pages/contents/[typeSlug]/[contentSlug].vue
+++ app/pages/contents/[typeSlug]/[contentSlug].vue
@@ -124,7 +124,9 @@
 
         <section>
             <div>
-                <h1 class="dark:text-white font-bold text-2xl"> {{ content?.title ?? 'unknown' }} </h1>
+                <h1 
+                    v-gsap.whenVisible.animateText.once.fast
+                    class="dark:text-white font-bold text-2xl"> {{ content?.title ?? 'unknown' }} </h1>
                 <p class="inline-flex items-center gap-2 dark:text-white">
                     <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar1-icon lucide-calendar-1"><path d="M11 14h1v4"/><path d="M16 2v4"/><path d="M3 10h18"/><path d="M8 2v4"/><rect x="3" y="4" width="18" height="18" rx="2"/></svg>
                         <span class="block text-sm font-light"> Publicado el {{ content?.created_at ?? '--/--/--'}} </span>
