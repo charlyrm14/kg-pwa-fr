88ceea90e4e6229892994715a450f7889ce2900a
charlyrm14 <charlyrm14@gmail.com>
2025-12-16 13:40:31 -0600

    feat(ui-contents-redesign): se modifica ui de seccion de tipos de contenido

--- app/components/user/contents/ContentItem.vue
+++ app/components/user/contents/ContentItem.vue
@@ -11,28 +11,29 @@
 </script>
 
 <template>
-    <NuxtLink
-        :to="`/contents/${route.params.typeSlug}/${content?.slug}`">
-            <div class="p-3 flex justify-between gap-x-4 items-center cursor-pointer rounded-lg bg-white hover:opacity-75 dark:bg-dark-light dark:hover:bg-dark-extralight my-4">
-                <div class="group overflow-hidden rounded-full aspect-square w-25 transition-all duration-300 hover:bg-gray-200/30 dark:hover:bg-dark-light/40">
-                    <img :src="contentTypeImage(content?.type)" :alt="content?.title ?? 'unknown'" class="w-full h-full object-cover"/>
-                </div>
-                <div class="border-b border-gray-200 dark:border-dark-soft w-full pb-2">
-                    <div class="flex justify-between">
-                        <div>
-                            <h1 class="text-pink-500">
-                                {{ content?.title.slice(0, 40)  ?? 'unknown' }} ...
-                            </h1>
-                            <p class="my-1 text-gray-600 dark:text-gray-300 text-xs md:text-base">
-                                {{ content?.content.slice(0, 70) ?? 'unknown' }} ...
-                            </p>
-                            <p class="inline-flex items-center gap-x-2 text-xs md:text-sm text-gray-400 font-light mt-1">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar1-icon lucide-calendar-1"><path d="M11 14h1v4"/><path d="M16 2v4"/><path d="M3 10h18"/><path d="M8 2v4"/><rect x="3" y="4" width="18" height="18" rx="2"/></svg>
-                                <span> {{ content?.created_at ?? 'unknown' }} </span>
-                            </p>
-                        </div>
-                    </div>
+    <div class="bg-white dark:bg-dark-light rounded-4xl p-3.5 shadow dark:shadow-none">
+        <div class="flex justify-between items-center gap-x-3">
+            <div class="flex gap-x-3">
+                <NuxtLink :to="`/contents/${route.params.typeSlug}/${content?.slug}`" class="w-40 md:w-30">
+                    <img :src="contentTypeImage(content?.type)" :alt="content?.title ?? 'unknown'" class="w-30 h-20 object-cover rounded-2xl brightness-60"/>
+                </NuxtLink>
+                <div>
+                    <NuxtLink :to="`/contents/${route.params.typeSlug}/${content?.slug}`">
+                        <h2 class="text-pink-500 text-sm md:text-base font-extrabold break-words md:hidden"> {{ content?.title.slice(0, 75)  ?? 'unknown' }} </h2>
+                    </NuxtLink>
+                    <NuxtLink :to="`/contents/${route.params.typeSlug}/${content?.slug}`">
+                        <h2 class="text-pink-500 text-sm md:text-base font-extrabold break-words hidden md:block"> {{ content?.title  ?? 'unknown' }} </h2>
+                    </NuxtLink>
+                    <span class="hidden md:block text-gray-400"> {{ content?.content.slice(0, 100) ?? 'unknown' }} ... </span>
+                    <p class="inline-flex items-center gap-x-2 text-xs md:text-sm text-gray-400 font-light mt-4 md:mt-2">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-calendar1-icon lucide-calendar-1"><path d="M11 14h1v4"/><path d="M16 2v4"/><path d="M3 10h18"/><path d="M8 2v4"/><rect x="3" y="4" width="18" height="18" rx="2"/></svg>
+                        <span> {{ content?.created_at ?? 'unknown' }} </span>
+                    </p>
                 </div>
             </div>
-    </NuxtLink>
+            <NuxtLink :to="`/contents/${route.params.typeSlug}/${content?.slug}`" class="text-gray-600 dark:text-white">
+                <svg xmlns="http://www.w3.org/2000/svg" width="34" height="34" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-chevron-right-icon lucide-chevron-right"><path d="m9 18 6-6-6-6"/></svg>
+            </NuxtLink>
+        </div>
+    </div>
 </template>
