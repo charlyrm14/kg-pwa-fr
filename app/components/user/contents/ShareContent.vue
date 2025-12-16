b0f7c651f5a283a063fe8a59417fd996383c9327
charlyrm14 <charlyrm14@gmail.com>
2025-12-16 16:56:53 -0600

    feat(ui-content-detail-redesign): se modifica ui de seccion de detalle de contenido

--- app/components/user/contents/ShareContent.vue
+++ app/components/user/contents/ShareContent.vue
@@ -9,11 +9,11 @@
 </script>
 
 <template>
-<div class="fixed bottom-0 left-0 right-0 bg-white dark:bg-dark-light rounded-t-2xl ring-1 ring-gray-300 dark:ring-dark-soft p-2 shadow-lg transform transition-transform duration-300 z-[9999] translate-y-0">
+<div class="fixed bottom-3 left-3 right-3 md:left-6 md:right-6 bg-white dark:bg-black rounded-4xl ring-1 ring-gray-300 dark:ring-black p-2 shadow-4xl dark:shadow-none transform transition-transform duration-300 z-[9999] translate-y-0">
         <div class="w-full h-65 md:h-70 mx-auto mb-4 p-4 md:p-8 ">
             
             <div class="flex justify-between items-center">
-                <h4 class="dark:text-white text-xl"> Enviar a </h4>
+                <h4 class="dark:text-white text-xl font-extrabold"> Compartir contenido </h4>
                 <button
                     @click="$emit('closeShareModal')"
                     class="dark:text-white cursor-pointer hover:opacity-75">
@@ -25,20 +25,20 @@
 
             <div class="mt-5 flex gap-x-4">
                 <div class="flex flex-col items-center">
-                    <button class="bg-green-500 p-3 rounded-full text-white cursor-pointer hover:opacity-75 shadow">
+                    <button class="bg-green-500 p-3 rounded-2xl text-white cursor-pointer hover:opacity-75 shadow">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 6.75c0 8.284 6.716 15 15 15h2.25a2.25 2.25 0 0 0 2.25-2.25v-1.372c0-.516-.351-.966-.852-1.091l-4.423-1.106c-.44-.11-.902.055-1.173.417l-.97 1.293c-.282.376-.769.542-1.21.38a12.035 12.035 0 0 1-7.143-7.143c-.162-.441.004-.928.38-1.21l1.293-.97c.363-.271.527-.734.417-1.173L6.963 3.102a1.125 1.125 0 0 0-1.091-.852H4.5A2.25 2.25 0 0 0 2.25 4.5v2.25Z" />
                         </svg>
                     </button>
-                    <span class="block mt-2 dark:text-white font-extralight"> WhatsApp </span>
+                    <span class="block mt-2 dark:text-gray-400 font-extralight"> WhatsApp </span>
                 </div>
                 <div class="flex flex-col items-center">
-                    <button class="bg-blue-500 p-3 rounded-full text-white cursor-pointer hover:opacity-75 shadow">
+                    <button class="bg-blue-500 p-3 rounded-2xl text-white cursor-pointer hover:opacity-75 shadow">
                         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-8">
                             <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
                         </svg>
                     </button>
-                    <span class="block mt-2 dark:text-white font-extralight"> Email </span>
+                    <span class="block mt-2 dark:text-gray-400 font-extralight"> Email </span>
                 </div>
             </div>
 
