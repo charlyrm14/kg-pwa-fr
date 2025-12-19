7dc78dbec381059734b8fa9eb3429d9bca194ed8
charlyrm14 <charlyrm14@gmail.com>
2025-12-18 19:13:22 -0600

    feat(content-filter): se agrega filtro de contenidos por tipo de contenido

--- app/layouts/admin.vue
+++ app/layouts/admin.vue
@@ -11,7 +11,7 @@
             <Header/>
             <div class="flex flex-1 overflow-hidden">
                 <Sidebar/>
-                <main class="flex-1 px-4 overflow-y-auto md:ml-64">
+                <main class="flex-1 overflow-y-auto md:ml-64">
                     <section aria-labelledby="main-content-heading" class="p-4 md:py-4 rounded-xl min-h-[calc(100vh-100px)]">
                         <slot/>
                     </section>
