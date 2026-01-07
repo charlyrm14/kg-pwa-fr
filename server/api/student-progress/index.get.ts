1ef095c3d8c0baca7c862d6b01aba3d0dcf3dffa
charlyrm14 <charlyrm14@gmail.com>
2026-01-06 18:08:35 -0600

    refactor(user-progress-url): se modifica url para progreso de usuario

--- server/api/student-progress/index.get.ts
+++ server/api/student-progress/index.get.ts
@@ -6,6 +6,6 @@ export default defineEventHandler(async(event) => {
     const config = useRuntimeConfig()
 
     return await $fetch<ApiResponse<StudentProgress>>(
-        `${config.public.apiBaseUrl}/student-progress/771f8eb5-a52e-40e4-af21-af947bf76048`
+        `${config.public.apiBaseUrl}/student-progress`
     )
 })
