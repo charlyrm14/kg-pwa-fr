2eed93c04a462c0b5211189e2935fe921a10217e
charlyrm14 <charlyrm14@gmail.com>
2026-01-22 11:51:30 -0600

    fix(request-permissions): se remueve composable para solicitar permiso de notificaciones

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt'],
+  modules: ['@pinia/nuxt', 'v-gsap-nuxt'],
   pinia: {
     /**
      * @default `['stores']`
